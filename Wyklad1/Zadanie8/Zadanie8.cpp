3beae422a5332b11ab2590b87239bdb30fc39bb2
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 18:16:15 +0200

    Done

--- Wyklad1/Zadanie8/Zadanie8.cpp
+++ Wyklad1/Zadanie8/Zadanie8.cpp
@@ -3,10 +3,31 @@
 
 int main()
 {
-    int const N = 10;
-    for (int i = 0; i < N; i++) {
+    int const N = 6;
+    int M = N / 2;
+    int i = 0;
+
+
+        for (; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                if (j < M) {
+                    std::cout << "X";
+                }
+                else
+                    std::cout << ".";
+            }
+            std::cout << std::endl;
+        }
+    for (int k = 0; k < M; k++) {
         for (int j = 0; j < N; j++) {
+            if (j < M) {
+                std::cout << ".";
+            }
+            else
+                std::cout << "X";
         }
         std::cout << std::endl;
     }
+    std::cout << std::endl;
+
 }
