9b28e4c3f900f392f60fb7bd4150693f35d02671
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 16:19:54 +0200

    Copy z 3

---
+++ Wyklad1/Zadanie4/Zadanie4.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+    int const N = 6;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (i > j) {
+                std::cout << ".";
+            }
+            else
+                std::cout << "X";
+        }
+        std::cout << std::endl;
+    }
+}
