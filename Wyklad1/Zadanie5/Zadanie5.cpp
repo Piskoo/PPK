f91cf9124f66a0d1db9508a13f8ad660403b40ca
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 16:48:27 +0200

    Pol klepsydry

--- Wyklad1/Zadanie5/Zadanie5.cpp
+++ Wyklad1/Zadanie5/Zadanie5.cpp
@@ -6,7 +6,12 @@ int main()
     for (int i = 0; i < N; i++) {
         for (int j = 0; j < N; j++)
         {
-            std::cout << "X";
+            int M = N / 2;
+
+            if ((i > j and j < N / 2) or (j > N / 2) and i < j)
+                std::cout << ".";
+            else 
+                std::cout << "X";
         }
         std::cout << std::endl;
     }
