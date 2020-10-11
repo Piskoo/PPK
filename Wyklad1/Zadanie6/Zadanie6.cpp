0165ff5e64d873cf25d40b7a852b8b64f14871b6
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 17:17:37 +0200

    Kwadrat

---
+++ Wyklad1/Zadanie6/Zadanie6.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    int const N = 10;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            std::cout << "X";
+        }
+        std::cout << std::endl;
+    }
+}
