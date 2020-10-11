4346de217835835590e75cb94840909e669604bc
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 16:27:28 +0200

    kwadrat

--- Wyklad1/Zadanie5/Zadanie5.cpp
+++ Wyklad1/Zadanie5/Zadanie5.cpp
@@ -2,5 +2,12 @@
 
 int main()
 {
-    
+    int const N = 7;
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++)
+        {
+            std::cout << "X";
+        }
+        std::cout << std::endl;
+    }
 }
