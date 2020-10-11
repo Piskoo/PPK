805f2cc031a7c3225b4b10a4bfbed4c8c4dc4045
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 17:54:56 +0200

    1/4 wiatraka

--- Wyklad1/Zadanie6/Zadanie6.cpp
+++ Wyklad1/Zadanie6/Zadanie6.cpp
@@ -3,10 +3,17 @@
 int main()
 {
     int const N = 10;
+    int M = N / 2;
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            std::cout << "X";
+        
+        for (int j = 0; j < M; j++) {
+            if ((j < M and i < M and i <= j) or (i >= M and j < M and j <= j - i))
+                std::cout << "X";
+            else
+                std::cout << ".";
         }
+
+
         std::cout << std::endl;
     }
 }
