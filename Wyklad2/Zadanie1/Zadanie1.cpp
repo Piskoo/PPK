9971845788c4dd1869151fa024ac5c44b8249157
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 18:48:21 +0200

    Done

--- Wyklad2/Zadanie1/Zadanie1.cpp
+++ Wyklad2/Zadanie1/Zadanie1.cpp
@@ -2,6 +2,22 @@
 
 int main()
 {
-    int const tab[10] = { 8, 7, 2, 1, 5, 6, 4, 9, 3, 0 };
+    int const N = 10;
+    int tab[N] = {8, 7, 2, 1, 5, 6, 4, 9, 3, 0};
+    int sort = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            if (tab[j] < tab[j + 1]) {
+                sort = tab[j];
+                tab[j] = tab[j + 1];
+                tab[j + 1] = sort;
+            }
+        }
+    }
+
+    for (int i = 0; i < N; i++) {
+        std::cout << tab[i] << " ";
+    }
 
 }
