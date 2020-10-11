34c8dd29acbbdb58cecf38acd7ac9f5e4ae94ce2
Sylwester Piskozub <sylwpis025@student.polsl.pl>
2020-10-11 16:09:11 +0200

    Done

--- Wyklad1/Zadanie2/Zadanie2.cpp
+++ Wyklad1/Zadanie2/Zadanie2.cpp
@@ -2,5 +2,16 @@
 
 int main()
 {
+    int const W = 5;
+    int const K = 8;
     
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < K; j++) {
+            if ((j % 2) ==  0) 
+                std::cout << "X";
+            else
+                std::cout << ".";
+        }
+        std::cout << std::endl;
+    }
 }
